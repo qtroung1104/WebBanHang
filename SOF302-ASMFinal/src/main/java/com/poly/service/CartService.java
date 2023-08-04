37ca5bc0315b405d30c135ac4926b7205ce4574b
qtroung1104 <phiquangtruong2001@gmail.com>
2023-08-05 00:13:00 +0700

    Hi

---
+++ SOF302-ASMFinal/src/main/java/com/poly/service/CartService.java
@@ -0,0 +1,22 @@
+package com.poly.service;
+
+import java.util.Collection;
+
+import com.poly.entity.*;
+
+public interface CartService {
+
+	Item add(Integer id);
+
+	void remove(Integer id);
+
+	Item update(Integer id, int qty);
+
+	void clear();
+
+	Collection<Item> getItems();
+
+	int getCount();
+
+	double getAmount();
+}
